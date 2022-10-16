d8c83904c67ab39133ed94e7399e7e5df536d39b
Keewon Shin <kevinkwshin@gmail.com>
2022-10-16 15:19:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ model2_prediction = np.array([0.3,0.6,0.2,0.1,0.1,0.9,0.23,0.7,0.9,0.4,0.77,0.3,
 # return auroc of each model and p-value of auroc comparison
 aucs, p_value = delong_roc_test(response, model1_prediction, model2_prediction)
 
-print(aucs, p_value)
->>> [0.96428571 0.73809524] 0.09452572880558979
+print(f' aucs : {aucs}, p_value : {p_value}') 
+>>> aucs : [0.96428571 0.73809524], p_value : 0.09452572880558979
