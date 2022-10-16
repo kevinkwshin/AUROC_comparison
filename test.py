3483cb746277716b03462ea1442634af2254ae22
Keewon Shin <kevinkwshin@gmail.com>
2022-10-16 15:16:06 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+from auroc_comparsion import *
+
+response = np.array([0,0,0,0,0,0,1,1,1,1,1,1,1]) # GT
+model1_prediction = np.array([0.1,0.2,0.05,0.3,0.1,0.6,0.6,0.7,0.8,0.99,0.8,0.67,0.5]) # prediction of model1
+model2_prediction = np.array([0.3,0.6,0.2,0.1,0.1,0.9,0.23,0.7,0.9,0.4,0.77,0.3,0.89]) # prediction of model2
+aucs, p_value = delong_roc_test(response, model1_prediction, model2_prediction)
+print(aucs, p_value)
